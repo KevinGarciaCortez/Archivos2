3e1ee66e697f45d6081ac72efd0c074adf7dd892
KevinGarciaCortez <nike_0316@hotmail.com>
2022-05-31 11:48:56 -0700

    1

---
+++ Archivo/Archivo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt",true);
+            string[] lineas =
+            {
+                "Esta es la nueva escritura",
+                "Fin txt"
+            };
+            foreach (string line in lineas)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close();
+            Console.WriteLine("Escribiendo...");
+            Console.ReadKey();
+        }
+    }
+}
